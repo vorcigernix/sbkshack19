0d0190fb9467abec5b2048a6e02b68360f833498
Adam Sobotka <vorcigernix@gmail.com>
2019-01-09 18:39:27 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense } from 'react';
+import { Grommet } from 'grommet';
+import Nav from './components/Nav';
+import { Router } from "@reach/router";
+import Loading from './Loading';
+
+const Home = React.lazy(() => import('./Home'));
+const Mentors = React.lazy(() => import('./Mentors'));
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      
+        <Suspense fallback={<Loading />}>
+          <Grommet theme={theme}>
+            <Nav />
+            <Router>
+            <Home path="/" />
+            <Mentors path="/mentors" />
+            </Router>
+          </Grommet>
+        </Suspense>
+      
       </div>
     );
   }
