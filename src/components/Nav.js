9307a0268412e0f4e83a057389671b73e236b177
Adam Sobotka <adam.sobotka@socialbakers.com>
2019-02-08 13:00:16 +0100

    nav gradient

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -55,7 +55,7 @@ class Nav extends Component {
                 direction="row"
                 align="start"
                 pad="medium"
-                background="linear-gradient(102.77deg, #F44336 , #E91E63, #9C27B0)"
+                background="radial-gradient(circle at 80%, #F44336, #9C27B0 100%)"
                 elevation="medium"
                 height="small"
               >
